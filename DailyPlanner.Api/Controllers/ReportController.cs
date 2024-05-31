2e5f827e06b8f0bb1e4ca6c115d5605c509af855
Ruben Rebbnik <mindlessmuse.666@gmail.com>
2024-05-31 13:09:58 +0300

    Initial commit

---
+++ DailyPlanner.Api/Controllers/ReportController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DailyPlanner.Api.Controllers;
+
+public class ReportController : ControllerBase
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ DailyPlanner.Domain/Interface/IEntityId.cs
@@ -0,0 +1,6 @@
+﻿namespace DailyPlanner.Domain.Interface;
+
+public interface IEntityId<T> where T : struct
+{
+    public T Id { get; set; }
+}
